c8d00ad2d9aec00d7bb242173c1f5e8771326ba2
Mehdi Maghlal <106619783+Mehdimouslim@users.noreply.github.com>
2023-03-18 22:27:14 +0100

    Add files via upload

---
+++ src/components/HomePage.js
@@ -0,0 +1,172 @@
+import React from 'react';
+import '../style.css'
+function HomePage() {
+  return (
+    <>
+   <div className="main-content">
+   <div className="content">
+            <h1>Make Your Home
+                <br/> Natural with our products.
+            </h1>
+            <p>we produce natural products, made in morocco,100% handmade wholesale and retail.</p>
+           
+          </div>
+        
+          </div>
+
+
+         
+        
+ 
+    
+    <div className="container">
+        <h3 className="text-center" style={{paddingTop: '30px;'}}>SERVICES WE OFFER</h3>
+    </div>
+    
+   
+    <div className="container">
+        <div className="row" style={{marginTop: '100px;'}}>
+            <div className="col-md-4 py-3 py-md-0">
+                <div className="card" id="tpc">
+                    <img src="./image/1679118409975.jpg" alt="" className="card image-top"/>
+                    <div className="card-img-overlay">
+                        <h4 className="card-titel">Basket Doum</h4>
+                     
+                    
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-4 py-3 py-md-0">
+                <div className="card" id="tpc">
+                    <img src="./image/download (5).jpeg" alt="" className="card image-top"/>
+                    <div className="card-img-overlay">
+                        <h4 className="card-titel">Basket Doum</h4>
+                      
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-4 py-3 py-md-0">
+                <div className="card" id="tpc">
+                    <img src="./image/1679118409975.jpg" alt="" className="card image-top"/>
+                    <div className="card-img-overlay">
+                        <h4 className="card-titel">Basket Doum</h4>
+                    
+                   
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div className="container">
+        <h3 className="text-center" style={{marginTop: '50px;'}}>TREANDLY PRODUCTS</h3>
+        <div className="row" style={{marginTop: '50px;'}}>
+            <div className="col-md-3 py-3 py-md-0">
+                <div className="card" id="c">
+                    <img src="./image/1679118409958.jpg" alt="" className="card image-top"/>
+                    <div className="card-body">
+                        <h3 className="card-titel text-center">Best Chair</h3>
+                        <p className="card-text text-center">$1000.50</p>
+                
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-3 py-3 py-md-0">
+                <div className="card" id="c">
+                    <img src="./image/1679118409958.jpg" alt="" className="card image-top"/>
+                    <div className="card-body">
+                        <h3 className="card-titel text-center">New Chair</h3>
+                        <p className="card-text text-center">$100.50</p>
+                     
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-3 py-3 py-md-0">
+                <div className="card" id="c">
+                    <img src="./image/1679118409958.jpg" alt="" className="card image-top"/>
+                    <div className="card-body">
+                        <h3 className="card-titel text-center">New Chair</h3>
+                        <p className="card-text text-center">$300.20</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-3 py-3 py-md-0">
+                <div className="card" id="c">
+                    <img src="./image/1679118409958.jpg" alt="" className="card image-top"/>
+                    <div className="card-body">
+                        <h3 className="card-titel text-center">Modern Chair</h3>
+                        <p className="card-text text-center">$500.50</p>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div className="row" style={{marginTop: '50px;'}}>
+            <div className="col-md-3 py-3 py-md-0">
+                <div className="card" id="c">
+                    <img src="./image/traditional-moroccan-stool-made-of-doum-palm-leaves-and-raw-wood_original.png" alt="" className="card image-top"/>
+                    <div className="card-body">
+                        <h3 className="card-titel text-center">Best Sofa</h3>
+                        <p className="card-text text-center">$200.50</p>
+                       
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-3 py-3 py-md-0">
+                <div className="card" id="c">
+                    <img src="./image/traditional-moroccan-stool-made-of-doum-palm-leaves-and-raw-wood_original.png" alt="" className="card image-top"/>
+                    <div className="card-body">
+                        <h3 className="card-titel text-center">Basket Doum</h3>
+                        <p className="card-text text-center">$100.50</p>
+                     
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-3 py-3 py-md-0">
+                <div className="card" id="c">
+                    <img src="./image/traditional-moroccan-stool-made-of-doum-palm-leaves-and-raw-wood_original.png" alt="" className="card image-top"/>
+                    <div className="card-body">
+                        <h3 className="card-titel text-center"> Chair</h3>
+                        <p className="card-text text-center">$150.50</p>
+               
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-3 py-3 py-md-0">
+                <div className="card" id="c">
+                    <img src="./image/traditional-moroccan-stool-made-of-doum-palm-leaves-and-raw-wood_original.png" alt="" className="card image-top"/>
+                    <div className="card-body">
+                        <h3 className="card-titel text-center">Chair</h3>
+                        <p className="card-text text-center">$150.50</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   
+    <div className="container">
+        <h1 className="text-center" style={{marginTop: '50px;'}}>ABOUT</h1>
+        <div className="row" style={{marginTop: '50px;'}}>
+            <div className="col-md-6 py-3 py-md-0">
+                <div className="card">
+                    <img src="./image/about.png" alt=""/>
+                </div>
+            </div>
+            <div className="col-md-6 py-3 py-md-0">
+                <p>Green Doum is a company that preduce natural product, mande in morroco, 100% handmade ,we can the best employers that love to be creative and give time to make the products perfectly , the price of our products is the best for you to get a perfect product with a perfect price, we make different products but the most of it is from rafia and smarand, it take a lot of time to make it perfect all that just to please our dear client that trust our product and we will always do our best to give them the best products.</p>
+             
+            </div>
+        </div>
+    </div>
+   
+   
+   
+    </>
+  )
+}
+
+export default HomePage
